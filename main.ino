4e266a4f07c55acd2cac08c59c634617beb6485f
Harrison Jones <harrisonhjones@gmail.com>
2015-10-14 23:06:56 -0700

    Working verion using TCP Client

--- main.ino
+++ main.ino
@@ -4,7 +4,7 @@ SYSTEM_MODE(MANUAL);
 
 
 
-char buffer[4][1000];
+char buffer[5][1000];
 unsigned int bufferIndex = 0;   // cols index
 unsigned char stateIndex = 0;   // rows index
 
@@ -13,17 +13,8 @@ String strRequestType;
 String strHostname;
 String strPath;
 
-#include "HttpClient.h"
-HttpClient http;
-// Headers currently need to be set at init, useful for API keys etc.
-http_header_t headers[] = {
-    //  { "Content-Type", "application/json" },
-    //  { "Accept" , "application/json" },
-    { "Accept" , "*/*"},
-    { NULL, NULL } // NOTE: Always terminate headers will NULL
-};
-http_request_t request;
-http_response_t response;
+// SYNTAX
+TCPClient client;
 
 void setup()
 {
@@ -63,64 +54,93 @@ void loop()
             {
                 buffer[stateIndex++][bufferIndex] = 0;  // Null terminiate the string; also increment the rows index
                 bufferIndex = 0;                        // Reset the cols index
-                if(stateIndex > 3)
+                if(stateIndex > 4)
                 {
-                    Serial.println("Application>\tVersion: 0.0.4");
+                    Serial.println("Application>\tVersion: 0.0.17");
                     Serial.print("Application>\tRequest Type: ");
                     Serial.println(buffer[0]);
-                    Serial.print("Application>\tHeader: ");
-                    Serial.println(buffer[1]);
                     Serial.print("Application>\tHostname: ");
-                    Serial.println(buffer[2]);
+                    Serial.println(buffer[1]);
                     Serial.print("Application>\tPath: ");
+                    Serial.println(buffer[2]);
+                    Serial.print("Application>\tHeader: ");
                     Serial.println(buffer[3]);
+                    Serial.print("Application>\tBody: ");
+                    Serial.println(buffer[4]);
 
-                    request.hostname = String(buffer[2]);
-                    request.port = 80;
-                    request.path = String(buffer[3]);
-
-                    // GET, POST, PUT, DELETE, PATCH
-                    http.get(request, response, headers);
-                    /*if(strRequestType == "get")
+                    Serial.println("Application>\tConnecting...");
+                    if (client.connect(buffer[1], 80))
                     {
-                        Serial.println("HTTPClient>\tSending GET request");
-                        http.get(request, response, headers);
+                        Serial.println("Application>\t\tConnected");                        
+                        // REQUEST_TYPE PATH HTTP/1.0
+                        client.print(buffer[0]);
+                        client.print(" ");
+                        client.print(buffer[2]);
+                        client.println(" HTTP/1.0");
+
+                        // Host: HOST
+                        client.print("Host: ");
+                        client.println(buffer[1]);
+
+                        if(buffer[3][0] != 0)   // If it isn't empty
+                        {
+                            Serial.println("Application>\t\tSending special headers");
+                            client.println(buffer[3]);
+                        }
+
+                        
+                        client.println("X-Bot: Particle Serial Proxy 0.0.17");
+                        client.println("X-Bot-Author: Harrison Jones (harrison@hhj.me)");
+                        
+                        
+
+                        if(buffer[4][0] != 0)   // If it isn't empty
+                        {
+                            // Calc true context length:
+                            int cLength = 0;
+                            while(buffer[4][cLength++] != 0)
+                            {}
+                            cLength--;
+
+                            client.println("Content-Type: application/x-www-form-urlencoded");
+
+                            client.println("Content-Length: ");
+                            client.println(cLength);
+
+                            client.println();
+                            Serial.print("Application>\t\tSending special body (length = ");
+                            Serial.print(cLength);
+                            Serial.println(")");
+
+                            client.println(buffer[4]);
+
+                            client.println();
+                        }
+                        else
+                        {
+                            client.println("Content-Length: 0");
+                            client.println();
+                        }
+
+
+
+                        Serial.println("Application>\t\tData sent");
                     }
-                    
-                    if(strRequestType == "post")
+                    else
                     {
-                        Serial.println("HTTPClient>\tSending POST request");
-                        http.post(request, response, headers);
+                        Serial.print("Application>\t\tError\tUnable to connect"); 
                     }
-
-                    if(strRequestType == "put")
-                    {
-                        Serial.println("HTTPClient>\tSending PUT request");
-                        http.put(request, response, headers);
-                    }
-
-                    if(strRequestType == "delete")
-                    {
-                        Serial.println("HTTPClient>\tSending DELETE request");
-                        http.del(request, response, headers);
-                    }    
-
-                    if(strRequestType == "patch")
-                    {
-                        Serial.println("HTTPClient>\tSending PATCH request");
-                        http.patch(request, response, headers);
-                    }   */
-
-                    Serial.print("Application>\tResponse status: ");
-                    Serial.println(response.status);
-
-                    Serial.print("Application>\tHTTP Response Body: ");
-                    Serial.println(response.body);
 
                     stateIndex = 0;
                 }
             }
         }
+    }
+
+    if (client.available())
+    {
+        char c = client.read();
+        Serial.print(c);
     }
 
 }
